145b72cfe6ba3befd585924e1744e212299e709f
lakatoa <lakatoa@natur.cuni.cz>
2025-03-23 01:35:11 +0100

    added new testcases

--- comparator_test.py
+++ comparator_test.py
@@ -1,5 +1,5 @@
 import unittest
-from binarySearchTree import TreeNode 
+from binarySearchTree import TreeNode, TreeRoot 
 
 comparator = lambda a, b: abs(a) - abs(b)
 
@@ -12,36 +12,41 @@ def f(left_subtree = None, right_subtree = None, node_value = None):
 class TestComparator(unittest.TestCase):
     def setUp(self):
         T = TreeNode
-        self.empty_tree = T(comparator=comparator)
-        self.single_node = T(-1, value="A", comparator=comparator)
-        self.tree = T(-5, "A", comparator=comparator,
+        self.empty_tree = TreeRoot(comparator=comparator)
+        self.single_node = TreeRoot(root = T(-1, value="A"), comparator=comparator)
+        self.tree = TreeRoot(root = T(-5, "A", 
                   left=T(-3, "B", T(-1, "C"), T(-4, "D")), 
-                  right=T(-8, "E", None, T(-9, "F")))
+                  right=T(-8, "E", None, T(-9, "F"))), comparator=comparator)
 
     def test_pre_order(self):
-        self.assertEqual(self.empty_tree.preorder(), [])
-        self.assertEqual(self.single_node.preorder(), [(-1, "A")])
-        self.assertEqual(self.tree.preorder(), [(-5, "A"), (-3, "B"), (-1, "C"), (-4, "D"), (-8, "E"), (-9, "F")])
+        self.assertEqual(self.empty_tree.getPreorder(), [])
+        self.assertEqual(self.single_node.getPreorder(), [(-1, "A")])
+        self.assertEqual(self.tree.getPreorder(), [(-5, "A"), (-3, "B"), (-1, "C"), (-4, "D"), (-8, "E"), (-9, "F")])
 
     def test_in_order(self):
-        self.assertEqual(self.empty_tree.inorder(), [])
-        self.assertEqual(self.single_node.inorder(), [(-1, "A")])
-        self.assertEqual(self.tree.inorder(), [(-1, "C"), (-3, "B"), (-4, "D"), (-5, "A"), (-8, "E"), (-9, "F")])
+        self.assertEqual(self.empty_tree.getInorder(), [])
+        self.assertEqual(self.single_node.getInorder(), [(-1, "A")])
+        self.assertEqual(self.tree.getInorder(), [(-1, "C"), (-3, "B"), (-4, "D"), (-5, "A"), (-8, "E"), (-9, "F")])
 
     def test_post_order(self):
-        self.assertEqual(self.empty_tree.postorder(), [])
-        self.assertEqual(self.single_node.postorder(), [(-1, "A")])
-        self.assertEqual(self.tree.postorder(), [(-1, "C"), (-4, "D"), (-3, "B"), (-9, "F"), (-8, "E"), (-5, "A")])
+        self.assertEqual(self.empty_tree.getPostorder(), [])
+        self.assertEqual(self.single_node.getPostorder(), [(-1, "A")])
+        self.assertEqual(self.tree.getPostorder(), [(-1, "C"), (-4, "D"), (-3, "B"), (-9, "F"), (-8, "E"), (-5, "A")])
+
+    def test_reverse_order(self):
+        self.assertEqual(self.empty_tree.getReverseorder(), [])
+        self.assertEqual(self.single_node.getReverseorder(), [(-1, "A")])
+        self.assertEqual(self.tree.getReverseorder(), [(-9, "F"), (-8, "E"), (-5, "A"), (-4, "D"), (-3, "B"), (-1, "C")])
 
     def test_level_order(self):
-        self.assertEqual(self.empty_tree.levelorder(), [])
-        self.assertEqual(self.single_node.levelorder(), [(-1, "A")])
-        self.assertEqual(self.tree.levelorder(), [(-5, "A"), (-3, "B"), (-8, "E"), (-1, "C"), (-4, "D"), (-9, "F")])
+        self.assertEqual(self.empty_tree.getLevelorder(), [])
+        self.assertEqual(self.single_node.getLevelorder(), [(-1, "A")])
+        self.assertEqual(self.tree.getLevelorder(), [(-5, "A"), (-3, "B"), (-8, "E"), (-1, "C"), (-4, "D"), (-9, "F")])
 
     def test_find_existing_key(self):
         func = lambda a, b: abs(a) - abs(b)
-        self.assertEqual(self.single_node.find(-1), TreeNode(-1, "A", comparator = comparator))
-        self.assertEqual(self.tree.find(-1), TreeNode(-1, "C", comparator = comparator))
+        self.assertEqual(self.single_node.find(-1), TreeNode(-1, "A"))
+        self.assertEqual(self.tree.find(-1), TreeNode(-1, "C"))
 
     def test_find_non_existing_key(self):
         self.assertIsNone(self.empty_tree.find(10))
@@ -78,27 +83,30 @@ class TestComparator(unittest.TestCase):
         self.empty_tree.clear()
         self.assertTrue(self.empty_tree.empty())
 
-    def test_previous(self):                                          #?????
-        self.assertIsNone(self.empty_tree.previous(10))
-        self.assertIsNone(self.single_node.previous(-1))
-        self.assertEqual(self.tree.previous(-4), TreeNode(-3, "B", TreeNode(-1, "C"), TreeNode(-4, "D"), comparator = comparator))
-        self.assertEqual(self.tree.previous(10), TreeNode(-9, "F", comparator = comparator))  # Key 10 is not in the tree ????
+    def test_previous(self):                                          
+        self.assertIsNone(self.empty_tree.previous(self.empty_tree.root, 10))
+        self.assertIsNone(self.single_node.previous(self.single_node.root, -1))
+        self.assertEqual(self.tree.previous(self.tree.root, -4), TreeNode(-3, "B", TreeNode(-1, "C"), TreeNode(-4, "D")))
+        self.assertEqual(self.tree.previous(self.tree.root, 10), TreeNode(-9, "F"))  # Key 10 is not in the tree ????
 
     def test_next(self):
-        self.assertIsNone(self.empty_tree.next(10))
-        self.assertIsNone(self.single_node.next(-1))
-        self.assertEqual(self.tree.next(-4), self.tree)
-        self.assertEqual(self.tree.next(0), TreeNode(-1, "C", comparator = comparator))  # Key 0 is not in the tree
+        self.assertIsNone(self.empty_tree.next(self.empty_tree.root, 10))
+        self.assertIsNone(self.single_node.next(self.single_node.root, -1))
+        self.assertEqual(self.tree.next(self.tree.root, -4), self.tree.root)
+        self.assertEqual(self.tree.next(self.tree.root, 0), TreeNode(-1, "C"))  # Key 0 is not in the tree
 
     def test_kth_element(self): 
         self.assertIsNone(self.empty_tree.kthLargestElement(10))
-        self.assertEqual(self.single_node.kthLargestElement(1), TreeNode(-1, "A", comparator = comparator))
-        self.assertEqual(self.tree.kthLargestElement(3), self.tree)  # 3rd largest element
+        self.assertEqual(self.single_node.kthLargestElement(1), TreeNode(-1, "A"))
+        self.assertEqual(self.tree.kthLargestElement(3), self.tree.root)  # 3rd largest element
+
+    def test_kth_smallest_element(self):
+        self.assertIsNone(self.empty_tree.kthSmallestElement(10))
+        self.assertEqual(self.single_node.kthSmallestElement(1), self.single_node.root)
+        self.assertEqual(self.tree.kthSmallestElement(3), TreeNode(-4, "D"))
 
     def test_element_access(self):
-        self.assertIsNone(self.empty_tree.elementAccess(10))
         self.assertEqual(self.single_node.elementAccess(-1), "A")
-        self.assertIsNone(self.single_node.elementAccess(10))  # Key is not in the tree
         self.assertEqual(self.tree.elementAccess(-3), "B")
         self.assertEqual(self.tree.elementAccess(-1), "C")  # Key is a leaf node
 
@@ -108,6 +116,34 @@ class TestComparator(unittest.TestCase):
         self.assertIsNone(self.single_node.getMetaValue(15, f))
         self.assertEqual(self.tree.getMetaValue(-5, f), "FEADBC")
         self.assertEqual(self.tree.getMetaValue(-3, f), "DBC")
+
+    def test_iter(self):
+        self.assertEqual([(key, value) for key, value in self.empty_tree], [])
+        self.assertEqual([(key, value) for key, value in self.single_node], [(-1, "A")])
+        self.assertEqual([(key, value) for key, value in self.tree], [(-1, "C"), (-3, "B"), (-4, "D"), (-5, "A"), (-8, "E"), (-9, "F")])
+
+    def test_eq(self):
+        T = TreeNode
+        tree1 = TreeRoot(root = T(-5, "A", 
+                  left=T(-3, "B", T(-1, "C"), T(-4, "D")), 
+                  right=T(-8, "E", None, T(-9, "F"))), comparator=comparator)
+        empty_tree = TreeRoot(comparator=comparator)
+        self.assertEqual(tree1, self.tree)
+        self.assertNotEqual(tree1, self.single_node)
+        self.assertNotEqual(tree1, self.empty_tree)
+        self.assertEqual(self.empty_tree, empty_tree)
+
+    def test_errors(self):
+        with self.assertRaises(KeyError):  
+            self.empty_tree.elementAccess(10)       
+            self.single_node.elementAccess(10)
+        invalid_object = "not a TreeRoot"
+        with self.assertRaises(TypeError):
+            self.tree == invalid_object
+            self.tree.kthLargestElement("a")
+            self.tree.kthSmallestElement("b")
+            self.tree.next(invalid_object, 5)
+            self.tree.previous(invalid_object, 5)
 
 
 if __name__ == "__main__":
